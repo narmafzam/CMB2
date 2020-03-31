e117f176a9ce967d418c2960e3568d410cfad43a
Farshad Asgharzade <Farshad_asgharzade@hotmail.com>
2020-03-31 21:17:11 +0430

    update mutlibase type class

--- includes/types/CMB2_Type_Multi_Base.php
+++ includes/types/CMB2_Type_Multi_Base.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CMB Multi base field type
  *
@@ -10,109 +11,165 @@
  * @license   GPL-2.0+
  * @link      https://cmb2.io
  */
-abstract class CMB2_Type_Multi_Base extends CMB2_Type_Base {
+abstract class CMB2_Type_Multi_Base extends CMB2_Type_Base
+{
 
 	/**
 	 * Generates html for an option element
 	 *
-	 * @since  1.1.0
-	 * @param  array $args Arguments array containing value, label, and checked boolean
+	 * @param array $args Arguments array containing value, label, and checked boolean
+	 *
 	 * @return string       Generated option element html
+	 * @since  1.1.0
 	 */
-	public function select_option( $args = array() ) {
-		return sprintf( "\t" . '<option value="%s" %s>%s</option>', $args['value'], selected( isset( $args['checked'] ) && $args['checked'], true, false ), $args['label'] ) . "\n";
+	public function select_option($args = [])
+	{
+		return sprintf("\t" . '<option value="%s" %s>%s</option>', $args['value'], selected(isset($args['checked']) && $args['checked'], true, false), $args['label']) . "\n";
+	}
+
+	/**
+	 * @param       $label
+	 * @param array $attrs
+	 * @param array $items
+	 *
+	 * @return string
+	 */
+	public function select_optgroup($label, $attrs = [], $items = [])
+	{
+		return sprintf("\t" . '<optgroup label="%s" %s>%s</optgroup>', $label, $this->concat_attrs($attrs), $items) . "\n";
 	}
 
 	/**
 	 * Generates html for list item with input
 	 *
-	 * @since  1.1.0
-	 * @param  array $args Override arguments
-	 * @param  int   $i    Iterator value
+	 * @param array $args Override arguments
+	 * @param int   $i Iterator value
+	 *
 	 * @return string       Gnerated list item html
+	 * @since  1.1.0
 	 */
-	public function list_input( $args = array(), $i ) {
-		$a = $this->parse_args( 'list_input', array(
+	public function list_input($args = [], $i)
+	{
+		$a = $this->parse_args('list_input', [
 			'type'  => 'radio',
 			'class' => 'cmb2-option',
 			'name'  => $this->_name(),
-			'id'    => $this->_id( $i ),
+			'id'    => $this->_id($i),
 			'value' => $this->field->escaped_value(),
 			'label' => '',
-		), $args );
+		], $args);
 
-		return sprintf( "\t" . '<li><input%s/> <label for="%s">%s</label></li>' . "\n", $this->concat_attrs( $a, array( 'label' ) ), $a['id'], $a['label'] );
+		return sprintf("\t" . '<li><input%s/> <label for="%s">%s</label></li>' . "\n", $this->concat_attrs($a, ['label']), $a['id'], $a['label']);
 	}
 
 	/**
 	 * Generates html for list item with checkbox input
 	 *
-	 * @since  1.1.0
-	 * @param  array $args Override arguments
-	 * @param  int   $i    Iterator value
+	 * @param array $args Override arguments
+	 * @param int   $i Iterator value
+	 *
 	 * @return string       Gnerated list item html
+	 * @since  1.1.0
 	 */
-	public function list_input_checkbox( $args, $i ) {
+	public function list_input_checkbox($args, $i)
+	{
 		$saved_value = $this->field->escaped_value();
-		if ( is_array( $saved_value ) && in_array( $args['value'], $saved_value ) ) {
+		if (is_array($saved_value) && in_array($args['value'], $saved_value)) {
 			$args['checked'] = 'checked';
 		}
 		$args['type'] = 'checkbox';
-		return $this->list_input( $args, $i );
+		return $this->list_input($args, $i);
 	}
 
 	/**
 	 * Generates html for concatenated items
 	 *
-	 * @since  1.1.0
-	 * @param  array $args Optional arguments
+	 * @param array $args Optional arguments
+	 *
 	 * @return string        Concatenated html items
+	 * @since  1.1.0
 	 */
-	public function concat_items( $args = array() ) {
+	public function concat_items($args = [])
+	{
 		$field = $this->field;
 
-		$method = isset( $args['method'] ) ? $args['method'] : 'select_option';
-		unset( $args['method'] );
+		$method = isset($args['method']) ? $args['method'] : 'select_option';
+		unset($args['method']);
 
 		$value = null !== $field->escaped_value()
 			? $field->escaped_value()
 			: $field->get_default();
 
-		$value = CMB2_Utils::normalize_if_numeric( $value );
+		$value = CMB2_Utils::normalize_if_numeric($value);
 
 		$concatenated_items = '';
-		$i = 1;
+		$i                  = 1;
 
-		$options = array();
-		if ( $option_none = $field->args( 'show_option_none' ) ) {
+		$options = [];
+		if ($option_none = $field->args('show_option_none')) {
 			$options[''] = $option_none;
 		}
-		$options = $options + (array) $field->options();
-		foreach ( $options as $opt_value => $opt ) {
+		$options = $options + (array)$field->options();
+		foreach ($options as $opt_value => $opt) {
 
-			// Clone args & modify for just this item
-			$a = $args;
-
-			$a['value'] = $opt_value;
 			if (is_array($opt)) {
+				$group_options = $opt;
+				if (isset($opt['options'])) {
 
-				foreach ($opt as $attr => $attr_value) {
-					$a[$attr] = $attr_value;
+					$group_options = $opt['options'];
+				}
+				if (is_array($group_options)) {
+
+					$items = '';
+					foreach ($group_options as $key => $item) {
+						$items .= $this->generate_option(
+							$method,
+							$args,
+							$item,
+							$key,
+							$i++
+						);
+					}
+					$label = isset($opt['label']) ? $opt['label'] : $opt_value;
+
+					$item = $this->select_optgroup($label, [], $items);
 				}
 			} else {
 
-				$a['label'] = $opt;
+				$item = $this->generate_option(
+					$method,
+					$args,
+					$opt,
+					$opt_value,
+					$i++
+				);
 			}
 
-			// Check if this option is the value of the input
-			if ( $value === CMB2_Utils::normalize_if_numeric( $opt_value ) ) {
-				$a['checked'] = 'checked';
-			}
-
-			$concatenated_items .= $this->$method( $a, $i++ );
+			$concatenated_items .= $item;
 		}
 
 		return $concatenated_items;
 	}
 
+	public function generate_option($method, $args, $option, $value, $index)
+	{
+		$args['value'] = $value;
+		if (is_array($option)) {
+
+			foreach ($option as $attr => $attr_value) {
+
+				$args[$attr] = $attr_value;
+			}
+		} else {
+
+			$args['label'] = $option;
+		}
+
+		// Check if this option is the value of the input
+		if ($value === CMB2_Utils::normalize_if_numeric($value)) {
+			$args['checked'] = 'checked';
+		}
+
+		return $this->$method($args, $index);
+	}
 }
