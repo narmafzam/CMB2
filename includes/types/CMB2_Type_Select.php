b968a3d5c8b502456209e9c735534590fd7dc271
Farshad <farshad_asgharzade@hotmail.com>
2020-11-25 12:27:34 +0330

    update select type

--- includes/types/CMB2_Type_Select.php
+++ includes/types/CMB2_Type_Select.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * CMB select field type
  *
- * @since  2.2.2
+ * @since     2.2.2
  *
  * @category  WordPress_Plugin
  * @package   CMB2
@@ -10,26 +11,29 @@
  * @license   GPL-2.0+
  * @link      https://cmb2.io
  */
-class CMB2_Type_Select extends CMB2_Type_Multi_Base {
-
-	public function render() {
-		$a = $this->parse_args( 'select', array(
+class CMB2_Type_Select extends CMB2_Type_Multi_Base
+{
+	
+	public function render()
+	{
+		$a = $this->parse_args('select', [
 			'class'   => 'cmb2_select',
 			'name'    => $this->_name(),
 			'id'      => $this->_id(),
-			'desc'    => $this->_desc( true ),
+			'desc'    => $this->_desc(true),
 			'options' => $this->concat_items(),
-		) );
-
+		]);
+		
 		if (isset($a['multiple'])
 			&& $a['multiple']) {
-
+			
 			$a['name'] = $a['name'] . '[]';
 		}
 		
-		$attrs = $this->concat_attrs( $a, array( 'desc', 'options' ) );
+		$attrs = $this->concat_attrs($a, ['desc', 'options']);
+		
 		return $this->rendered(
-			sprintf( '<select%s>%s</select>%s', $attrs, $a['options'], $a['desc'] )
+			sprintf('<select%s>%s</select>%s', $attrs, $a['options'], $a['desc'])
 		);
 	}
 }
